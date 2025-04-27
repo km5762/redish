b1e04a24deb103d4105b179e673daf0738088cf4
km5762 <kumyles@gmail.com>
2025-04-27 13:35:33 -0400

    RespCodec::decode_string handling empty strings/no crlf

--- src/resp_codec.cpp
+++ src/resp_codec.cpp
@@ -9,21 +9,37 @@
 
 std::optional<RespCodec::Message> RespCodec::decode(const std::string_view data) {
     m_data = data;
+    std::optional<Message> message{};
     switch (advance()) {
         case '+':
-            return decode_simple_string();
+            message = decode_simple_string();
+            break;
         default:
-            return std::nullopt;
+            message = std::nullopt;
     }
+
+    // if we have not consumed all the input
+    if (m_position != data.size()) {
+        return std::nullopt;
+    }
+    return message;
 }
 
 std::optional<RespCodec::SimpleString> RespCodec::decode_simple_string() {
-    size_t i{0};
-    while (!(m_data[i] == '\r' && m_data[i + 1] == '\n')) {
+    size_t i{m_position};
+
+    while (true) {
+        if (i + 1 >= m_data.size()) {
+            return std::nullopt;
+        }
+        if (m_data[i] == '\r' && m_data[i + 1] == '\n') {
+            break;
+        }
         ++i;
     }
 
     const size_t size{i - m_position};
     const std::string s{m_data.data() + m_position, size};
+    m_position = i + 2;
     return SimpleString{s};
 }
--- tests/resp_codec.cpp
+++ tests/resp_codec.cpp
@@ -12,3 +12,27 @@ TEST_F(RespCodecTest, HappyPath) {
     EXPECT_EQ("OK", std::get<RespCodec::SimpleString>(*output).value);
 }
 
+TEST_F(RespCodecTest, EmptyInvalidString) {
+    const auto output = m_codec.decode("");
+    EXPECT_EQ(std::nullopt, output);
+}
+
+TEST_F(RespCodecTest, NoCrlf) {
+    const auto output = m_codec.decode("+");
+    EXPECT_EQ(std::nullopt, output);
+}
+
+TEST_F(RespCodecTest, CrNoLf) {
+    const auto output = m_codec.decode("+\r");
+    EXPECT_EQ(std::nullopt, output);
+}
+
+TEST_F(RespCodecTest, EmptyValidString) {
+    const auto output = m_codec.decode("+\r\n");
+    EXPECT_EQ("", std::get<RespCodec::SimpleString>(*output).value);
+}
+
+TEST_F(RespCodecTest, TrailingInput) {
+    const auto output = m_codec.decode("+\r\nTEST");
+    EXPECT_EQ(std::nullopt, output);
+}
